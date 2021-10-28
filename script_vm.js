7f13a377b2f4184594fc00f6acc76e928367166b
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-10-27 19:28:40 -0700

    committing addition of Daniel's API

--- script_vm.js
+++ script_vm.js
@@ -1,24 +1,50 @@
-var fetchBtn = document.getElementById('fetch-button');
-var wikiURL = "https://en.wikipedia.org/w/api.php?&origin=*&action=parse&format=json&page=Wonders_of_the_World&prop=wikitext&formatversion=2"
-//var wikiURL = "https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Most%20&limit=5"
-//var wikiURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Craig%20Noone&format=json&origin=*";
-// must add &origin=* to end of URL to avoid CORS block
+// var fetchBtn = document.getElementById('fetch-button');
+// var wikiURL = "https://en.wikipedia.org/w/api.php?&origin=*&action=parse&format=json&page=Wonders_of_the_World&prop=wikitext&formatversion=2"
+// //var wikiURL = "https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Most%20&limit=5"
+// //var wikiURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Craig%20Noone&format=json&origin=*";
+// // must add &origin=* to end of URL to avoid CORS block
 
-function getAPI() {
-    console.log("successful click");
-    fetch (wikiURL)
-        .then(function(result){
-            console.log(result);
-            return result.json();
-        })
-        .then(function(data){
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.error(error);
+// function getAPI() {
+//     console.log("successful click");
+//     fetch (wikiURL)
+//         .then(function(result){
+//             console.log(result);
+//             return result.json();
+//         })
+//         .then(function(data){
+//             console.log(data);
+//         })
+//         .catch(function (error) {
+//             console.error(error);
+//     });
+// }
+
+// fetchBtn.addEventListener('click', getAPI)
+
+// document.getElementById('fetch-button').addEventListener('click', getAPI);
+
+var fetchBtn = document.getElementById('fetch-button');
+
+function fetchAPI() {
+    console.log("test click");
+    fetch("https://shazam.p.rapidapi.com/auto-complete?term=stronger&locale=en-US", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "shazam.p.rapidapi.com",
+            "x-rapidapi-key": "ca8d7029bdmsh4c7f8dda0605b77p16f6fcjsnfbf2331b00a0"
+        }
+    })
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.hints[0].term);
+    })
+    .catch(err => {
+        console.error(err);
     });
 }
 
-fetchBtn.addEventListener('click', getAPI)
-
-// document.getElementById('fetch-button').addEventListener('click', getAPI);
+fetchBtn.addEventListener('click', fetchAPI);
