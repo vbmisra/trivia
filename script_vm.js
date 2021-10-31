c2addf5cefa4a1ac0abe23a8ce1196de185dd767
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-10-31 11:01:10 -0700

    committing changes to script_vm

--- script_vm.js
+++ script_vm.js
@@ -1,8 +1,4 @@
-// Buttons
-//var wikiBtn = document.getElementById('wiki-button');
-//var pokeBtn = document.getElementById('pokemon-button');
-//var musicBtn = document.getElementById('music-button');
-//var nextBtn = document.querySelector('#nextQuestion');
+// Defining elements from questions.html
 var choiceA = document.querySelector('#ansBtn1');
 var choiceB = document.querySelector('#ansBtn2');
 var choiceC = document.querySelector('#ansBtn3');
@@ -11,22 +7,25 @@ var questionEl = document.querySelector('#questionTitle');
 var nextBtn = document.querySelector('#nextQuestion');
 var alertEl = document.querySelector('#alertUser');
 
-// URLs
+// API URLs
 var wikiURL = "https://en.wikipedia.org/w/api.php?&origin=*&action=parse&format=json&page=Wonders_of_the_World&prop=wikitext&formatversion=2";
 var pokemonUrl = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20";
 var musicURL = "https://shazam.p.rapidapi.com/auto-complete?term=love&locale=en-US";
 
+// Emtpy arrays to be populated by 
 var pokeAnswers = [];
 var musicAnswers = [];
+
+// Variable definitions
 var pokeIndex = 0;
 var musicIndex = 0;
 var currentQuestion = 0;
-var lastQuestion = 10;
 var playerOneScore = 0;
 var playerTwoScore = 0;
 
 nextBtn.addEventListener('click', nextQuestion);
 
+// function to retrieve wikipedia API
 function getWikiAPI() {
     console.log("successful click");
     fetch (wikiURL)
@@ -42,15 +41,14 @@ function getWikiAPI() {
     });
 }
 
-//function getMusicAPI() {
-    //console.log("test click");
-    fetch(musicURL, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "shazam.p.rapidapi.com",
-            "x-rapidapi-key": "ca8d7029bdmsh4c7f8dda0605b77p16f6fcjsnfbf2331b00a0"
-        }
-    })
+// retrieves music API, populates question and answer arrays with retreived data
+fetch(musicURL, {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "shazam.p.rapidapi.com",
+        "x-rapidapi-key": "ca8d7029bdmsh4c7f8dda0605b77p16f6fcjsnfbf2331b00a0"
+    }
+})
     .then(response => {
         console.log(response);
         return response.json();
@@ -64,32 +62,27 @@ function getWikiAPI() {
         console.log(musicAnswers);
         if (currentQuestion < 5) {
         makeMusicQuestions(musicAnswers, musicIndex);
-        }
+    }
+})
+    
+// retrieves pokemon API, populates question and answer arrays with retreived data
+fetch(pokemonUrl)
+    .then(function (response) {
+        console.log(response);
+        return response.json();
     })
-    // .catch(err => {
-    //     console.error(err);
-    // });
-//}
-
-//function getPokeAPI() {
-    fetch(pokemonUrl)
-        .then(function (response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data);
-            for(i=0; i<data.results.length; i++) {
-                pokeAnswers.push(data.results[i].name);
-            }
-            console.log(pokeAnswers);
-            if (currentQuestion >= 5 && currentQuestion < 10) {
-            makePokemonQuestions(pokeAnswers, pokeIndex);
-            }
-        })
-        //return pokeAnswers;
-//}
-
+    .then(function(data){
+        console.log(data);
+        for(i=0; i<data.results.length; i++) {
+            pokeAnswers.push(data.results[i].name);
+        }
+        console.log(pokeAnswers);
+        if (currentQuestion >= 5 && currentQuestion < 10) {
+        makePokemonQuestions(pokeAnswers, pokeIndex);
+        }
+})
+    
+// function to make pokemon questions and answers
 function makePokemonQuestions(arrPokemonNames, pokeIndex){
     var pokeQuestions = [
         {
@@ -134,6 +127,7 @@ function makePokemonQuestions(arrPokemonNames, pokeIndex){
     console.log(pokeQuestions)
 }
 
+// function to make music questions and answers
 function makeMusicQuestions(arrMusic, musicIndex){
     var musicQuestions = [
         {
@@ -179,6 +173,7 @@ function makeMusicQuestions(arrMusic, musicIndex){
     console.log(musicQuestions)
 }
 
+// moves to next question
 function nextQuestion() {
     currentQuestion++;
     if(currentQuestion < 5) {
@@ -194,12 +189,15 @@ function nextQuestion() {
     }
 }
 
+// checks answer based on user click
 function checkAnswer(answer) {
     if (currentQuestion < 5) {
         if (answer == musicAnswerKey[musicIndex].Correct) {
+            console.log(choiceA.textContent);
             alertEl.textContent = 'Correct!'
             playerOneScore++;
         } else {
+            console.log(choiceA.textContent);
             alertEl.textContent = 'Wrong!'
         }
     } else if (currentQuestion > 4 && currentQuestion < 10) {
@@ -211,68 +209,26 @@ function checkAnswer(answer) {
             alertEl.textContent = 'Wrong!'
         }
     }
-    // if (answer == questions[currentQuestion].correct) {
-    //     score++;
-    //     // display "correct"
-    // } //else {
-    // //     display "wrong"
-    // // }
-    // count = 0;
-    // if (currentQuestion <= lastQuestion) {
-    //     currentQuestion++;
-    //     //displayQuestion(); don't run in case we want to click "next question" to move on
-    // } else {
-    //     clearInterval(timer); // need to change depending on what Juwon defined
-    //     displayScore();
-    // }
-}
-
-function test() {
-    //event.preventDefault();
-    console.log('hello');
 }
 
 //getWikiAPI();
-//getPokeAPI();
-//getMusicAPI();
-//console.log(getPokeAPI());
-// questions
-// var wikiQuestions = [];
+
+//answer keys for pokemon and music
 var pokeAnswerKey = [
     {
         question: "Which pokemon evolves into Raichu?",
-        A: pokeAnswers[0],
-        B: pokeAnswers[4],
-        C: pokeAnswers[6],
-        D: pokeAnswers[8],
         Correct: "B"
     }, {
         question: "Which pokemon can use flamethrower?",
-        A: pokeAnswers[2],
-        B: pokeAnswers[5],
-        C: pokeAnswers[15],
-        D: pokeAnswers[19],
         Correct: "C"
     }, {
         question: "Which pokemon is famous for singing in the show?",
-        A: pokeAnswers[18],
-        B: pokeAnswers[7],
-        C: pokeAnswers[10],
-        D: pokeAnswers[11],
         Correct: "A"
     }, {
         question: "Which pokemon looks like a cobra?",
-        A: pokeAnswers[3],
-        B: pokeAnswers[2],
-        C: pokeAnswers[14],
-        D: pokeAnswers[16],
         Correct: "A"
     }, {
         question: "Which pokemon can fly?",
-        A: pokeAnswers[17],
-        B: pokeAnswers[12],
-        C: pokeAnswers[13],
-        D: pokeAnswers[1],
         Correct: "D"
     }
 ];
@@ -281,89 +237,19 @@ var pokeAnswerKey = [
 var musicAnswerKey = [
     {
         question: "Fill in the blank: Drake's newest album is called Certified _____",
-        A: musicAnswers[9],
-        B: musicAnswers[8],
-        C: musicAnswers[6],
-        D: musicAnswers[3],
-        Correct: "B"
+        Correct: "A"
     }, {
         question: "Which song is by the Jonas Brothers?",
-        A: musicAnswers[2],
-        B: musicAnswers[8],
-        C: musicAnswers[6],
-        D: musicAnswers[3],
-        Correct: "C"
+        Correct: "B"
     }, {
         question: "Which of these is a song by Ed Sheeran?",
-        A: musicAnswers[7],
-        B: musicAnswers[5],
-        C: musicAnswers[0],
-        D: musicAnswers[3],
         Correct: "B"
     }, {
         question: "Chief Keef rose to fame after this popular song:",
-        A: musicAnswers[9],
-        B: musicAnswers[8],
-        C: musicAnswers[7],
-        D: musicAnswers[3],
         Correct: "D"
     }, {
         question: "This song by Ckay rose to fame on Tik Tok:",
-        A: musicAnswers[2],
-        B: musicAnswers[4],
-        C: musicAnswers[0],
-        D: musicAnswers[3],
         Correct: "C"
     }
 ];
-
-// var questions = [];
-// Array.prototype.push.apply(questions, pokeQuestions, musicQuestions);
-
-// var currentQuestion = 0;
-// var lastQuestion = questions.length - 1;
-// var questionEl = document.querySelector("#question"); // need to change based on Hekmat's title
-
-//answer choice buttons - might need to change depending on what hekmat defined
-// var choiceA = document.querySelector('#choiceA');
-// var choiceB = document.querySelector('#choiceB');
-// var choiceC = document.querySelector('#choiceC');
-
-// choiceA.addEventListener('click', checkAnswer('A'));
-// choiceB.addEventListener('click', checkAnswer('B'));
-// choiceC.addEventListener('click', checkAnswer('C'));
-// nextBtn.addEventListener('click', nextQuestion);
-
-// must add &origin=* to end of URL to avoid CORS block
-
-
-// wikiBtn.addEventListener('click', getWikiAPI);
-// pokeBtn.addEventListener('click', generateQuestions);
-// musicBtn.addEventListener('click', getMusicAPI);
-
-// function displayQuestion() {
-//     if(currentQuestion <= lastQuestion) {
-//         var quest = questions[currentQuestion];
-//         questionEl.textContent = quest.question;
-//         choiceA.textContent = quest.choiceA;
-//         choiceB.textContent = quest.choiceB;
-//         choiceC.textContent = quest.choiceC;
-//     } else {
-//         clearInterval(timer); // need to change depending on what Juwon defined
-//         displayScore();
-//     }
-// }
-
-
-// function nextQuestion() {
-//     if (currentQuestion <= lastQuestion) {
-//         currentQuestion++;
-//         displayQuestion();
-//     } else {
-//         clearInterval(timer);
-//         displayScore();
-//     }
-// }
-
-
 
